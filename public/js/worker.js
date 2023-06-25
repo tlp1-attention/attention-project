5b0fd0aa83e1e5a849b4c39ea9214d41473b9e15
Dante Benitez <benitezdante123@gmail.com>
2023-06-25 12:54:19 -0300

    Arreglado worker de notificaciones

--- public/js/worker.js
+++ public/js/worker.js
@@ -2,9 +2,9 @@ self.addEventListener('push', async (event) => {
 
     const { data } = event;
 
-    const pushMessage = await data.json();
- 
-    event.waitUntil(self.Notification(pushMessage.title, {
+    const pushMessage = data.json();
+
+    event.waitUntil(self.registration.showNotification(pushMessage.title, {
         body: pushMessage.message,
         icon: '/assets/logo-1.png'
     }));
