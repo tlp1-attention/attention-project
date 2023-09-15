38b86242d52c605670389b42334c48be3f457c10
danteBenitez <benitezdante123@gmail.com>
2023-09-15 13:59:58 -0300

    chores: Añadido soporte para archivos de logs de peticiones

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@ import connectSQLite from 'connect-sqlite3';
 
 import loginRouter from './routes/auth.routes'
 import staticServer from './middleware/__server-static.middleware';
-import { loggingMiddleware } from './middleware/logging'
+import { logRequests } from './middleware/logging'
 import indexRouter from './routes/index.routes'
 import workSpaceRouter from './routes/workspace.routes'
 import eventRouter from './routes/events.routes'
@@ -34,7 +34,7 @@ app.set('view engine', 'ejs');
 
 // Library Middleware
 app.use(express.json());
-app.use(loggingMiddleware);
+app.use(logRequests);
 app.use(cors());
 app.use(helmet({
     contentSecurityPolicy: false // Allow CDN's resources to be delivered
@@ -60,10 +60,7 @@ app.use(session({
 
 // Custom middleware
 app.use(staticServer);
-app.use((req, res, next) => {
-  console.log(req.headers);
-  next();
-})
+
 // Routes
 app.use(loginRouter);
 app.use(indexRouter);
---
+++ src/utils/get-stream-for-log.ts
@@ -0,0 +1,43 @@
+import { createWriteStream } from "fs";
+import { mkdir, stat, writeFile } from "fs/promises";
+import { join } from "path";
+
+const streams = {};
+
+const getFormattedDate = (date: Date) => {
+  // Date format: YYYY-mm-dd
+  return date.toISOString().slice(0, 10);
+};
+
+export async function getStreamForLogFile(logFolder: string, suffix: string) {
+  const date = getFormattedDate(new Date());
+  const dir = join(logFolder, `${suffix}`);
+
+  await mkdir(dir, {
+    // Create `logs` if it does not exist,
+    // and does not fail if the desired directory 
+    // already exists
+    recursive: true,
+  });
+
+  const lastStream = streams[suffix];
+  const filepath = join(dir, `${date}-${suffix}.log`);
+
+  try {
+    const _stats = await stat(filepath);
+
+    if (lastStream) return lastStream;
+  } catch (err) {
+    if (err.code == "ENOENT") {
+      await writeFile(filepath, "");
+    } else {
+      throw new Error("Error inesperado al generar archivo de logs");
+    }
+  }
+
+  streams[suffix] = createWriteStream(filepath, {
+    flags: "a",
+  });
+
+  return streams[suffix];
+}
