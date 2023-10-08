a29efe1ae880225647c852dcb9c3b8e56524b641
Dante Benitez <benitezdante123@gmail.com>
2023-10-08 11:41:25 -0300

    feat: Implementada vista estática de lista de lecturas

--- src/controllers/workspace.controller.ts
+++ src/controllers/workspace.controller.ts
@@ -7,7 +7,7 @@ function renderTimer(req: AuthRequest, res: Response) {
         title: 'Espacio de trabajo',
         headerContentPartial: 'partials/timer-head.ejs',
         mainContentPartial: 'partials/timer.ejs',
-        username: req.user?.name || 'Nombre de usuario',
+        username: req.user?.name || 'Usuario',
     })
 }
 
@@ -16,16 +16,16 @@ function renderEvents(req: AuthRequest, res: Response) {
     res.render('layout-events', {
         title: 'Agenda',
         mainContentPartial: 'partials/events.ejs',
-        username: req.user?.name || 'Nombre de usuario',
+        username: req.user?.name || 'Usuario',
     })
 }
 
-function renderReading(req: AuthRequest, res: Response) {
+function renderReadingList(req: AuthRequest, res: Response) {
     res.render('layout-readings', {
         title: 'Lectura',
-        mainContentPartial: 'partials/.ejs',
-        username: req.user?.name || 'Nombre de usuario',
+        mainContentPartial: 'partials/reading-list.ejs',
+        username: req.user?.name || 'Usuario',
     })
 }
 
-export { renderEvents, renderReading, renderTimer }
+export { renderEvents, renderReadingList, renderTimer }
--- src/routes/workspace.routes.ts
+++ src/routes/workspace.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express'
-import { renderTimer, renderEvents } from '../controllers/workspace.controller' 
+import { renderTimer, renderEvents, renderReadingList } from '../controllers/workspace.controller' 
 
 const router = Router();
 
 router.get('/workspace/events' , renderEvents);
 router.get('/workspace/timer', renderTimer);
+router.get('/workspace/readings', renderReadingList);
 
 export default router;
