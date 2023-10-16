4f8cbea475a1f83f5263ddddf22b8dd9290f0f7b
Dante Benitez <benitezdante123@gmail.com>
2023-10-16 01:08:00 -0300

    feat: Arreglado display de nombre de usuario en Navbar

--- public/js/nav-bar.js
+++ public/js/nav-bar.js
@@ -1,6 +1,7 @@
 const slideButtons = document.querySelectorAll('.slide-button');
 const offcanvas = document.querySelector('#offcanvas');
 const lateralBar = document.querySelector('.lateral-bar');
+const username = document.querySelector('#username');
 
 slideButtons.forEach(btn => {
     btn.addEventListener('click', (_event) => {
@@ -9,3 +10,29 @@ slideButtons.forEach(btn => {
         capture: true
     })
 });
+
+const token = localStorage.getItem('token');
+
+document.addEventListener('DOMContentLoaded', async () => {
+    try {
+        const response = await fetch('/api/users', {
+            headers: {
+                'Authorization': token
+            }
+        });
+
+        if (!response.ok) throw response;
+
+        const { user } = await response.json();
+
+        username.innerHTML = user.name;
+
+    } catch(err) {
+        console.error(err);
+        Swal.fire({
+            icon: 'error',
+            title: 'Hubo un error al obtener información del usuario',
+            footer: 'Si el error persiste, contacte a los desarrolladores del sitio.'
+        });
+    }
+})
