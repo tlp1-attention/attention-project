304b74205d858089a435fec83eeb1a26d42b4e67
danteBenitez <benitezdante123@gmail.com>
2023-11-13 16:45:35 -0300

    feat: Implementado servicio de socket.io

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,25 @@
+import { createServer } from 'http';
 import app from './app';
 import env from './config/env';
 import setupDatabase from './database/setup';
 import { eventService } from './services/event.service';
+import { socketServerFrom } from './services/socket.service';
 import { scheduleReminders } from './utils/schedule-reminder';
 
 const PORT = env.PORT;
 
-
-
 // Export a listening server for testing
-export const server = app.listen(PORT, async () => {
+const httpServer = createServer(app);
+const socketServer = socketServerFrom(httpServer);
+socketServer.run();
+
+export const server = httpServer.listen(PORT, async () => {
     await eventService.getCountByWeek(2);
     await scheduleReminders();
     await setupDatabase();
+
+    
+
     console.log(`Server listening in: http://localhost:${PORT}`);
 });
+
--- src/services/socket.service.ts
+++ src/services/socket.service.ts
@@ -1,36 +1,62 @@
-import { Server as HttpServer } from "http";
-import { Server } from 'socket.io';
-import { UserService } from "./user.service";
+import { Server as HttpServer } from 'http'
+import { Server, Socket } from 'socket.io'
+import { UserService } from './user.service'
+
+type SocketMiddleware = (socket: Socket, next: Function) => void
 
 /**
  * Service that handles incoming socket
  * connections using socket.io.
  * Exposes an interface to send real-time
- * notifications to clients 
+ * notifications to clients
  */
 export class SocketService {
-    server: Server;
+    server: Server
+    middleware: SocketMiddleware[] = [];
 
+    /**
+     *
+     * @param usersService
+     * @param httpServer
+     */
     constructor(
-        private usersService: UserService,
         private httpServer: HttpServer
     ) {
         this.server = new Server(httpServer, {
             cors: {
-                origin: '*'
-            }
-        });
+                origin: '*',
+            },
+        })
     }
 
     /**
      * Starts listening for connections
      */
     async run() {
+        this.middleware.forEach((m) => this.server.use(m))
         this.server.on('connection', this.getConnectionHandler());
     }
 
     /**
-     * 
+     * Returns the handler used for connections
      */
+    getConnectionHandler() {
+        return (socket: Socket) => {
+            console.log('Se ha conectado un usuario...', socket.id);
+        }
+    }
 
+    /**
+     * Registers middleware to use for the socket
+     * server
+     *
+     * @param middleware
+     */
+    async useMiddleware(middleware: SocketMiddleware) {
+        this.middleware.push(middleware)
+    }
+}
+
+export function socketServerFrom(httpServer: HttpServer) {
+    return new SocketService(httpServer);
 }
