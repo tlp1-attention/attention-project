ce3d5ab8027616bb8803130fed0d411525de46e7
Dante Benitez <benitezdante123@gmail.com>
2023-11-26 23:01:22 -0300

    fix: Merge incompleto

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { createServer } from 'http'
 import app from './app'
 import env from './config/env'
@@ -7,16 +6,9 @@ import { setupLogger } from './services/logger.service'
 import { notificationService } from './services/notification.service'
 import { socketService } from './services/socket/socket.service'
 import { scheduleReminders } from './utils/schedule-reminder'
-=======
-import app from './app';
-import env from './config/env';
-import setupDatabase from './database/setup';
-import { sequelize } from './db';
->>>>>>> 7fbfb1a9804b94b34aeaaf88830c651993f466d4
 
 const PORT = env.PORT
 
-<<<<<<< HEAD
 const httpServer = createServer(app)
 notificationService.attach(socketService)
 socketService.runOn(httpServer)
@@ -27,12 +19,3 @@ export const server = httpServer.listen(PORT, async () => {
     setupLogger()
     console.log(`Server listening in: http://localhost:${PORT}`)
 })
-=======
-// Export a listening server for testing
-export const server = app.listen(PORT, async () => {
-    await sequelize.sync({ force: false })
-    // await scheduleReminders();
-    await setupDatabase();
-    console.log(`Server listening in: http://localhost:${PORT}`);
-});
->>>>>>> 7fbfb1a9804b94b34aeaaf88830c651993f466d4
